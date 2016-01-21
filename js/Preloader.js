1c0413d5087a8d862407cdab9e69947d2e4d570b
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-21 18:57:49 -0500

    Add Thach on preloader

--- js/Preloader.js
+++ js/Preloader.js
@@ -6,6 +6,7 @@ preload.prototype = {
         this.game.load.image('mic', 'assets/images/mic_profile_icon.png');
         this.game.load.image('jen', 'assets/images/jen_profile_icon.png');
         this.game.load.image('mvd', 'assets/images/mvd_profile_icon.png');
+        this.game.load.image('tnh', 'assets/images/tnh_profile_icon.png');
     },
     create: function(){
         this.game.state.start("StartMenu");
--- js/StartMenu.js
+++ js/StartMenu.js
@@ -9,6 +9,7 @@ menu.prototype = {
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'mic');
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'jen');
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'mvd');
+        this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'tnh');
     },
     update: function(){
     },
