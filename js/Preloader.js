f7c27445cd42963a36d072c5e5dd96b6ef0cb4a1
Michael VanDaniker <mvandani@gmail.com>
2016-01-21 18:56:14 -0500

    Adds MVD to the preloader

--- js/Preloader.js
+++ js/Preloader.js
@@ -4,6 +4,7 @@ var preload = function(game){
 preload.prototype = {
     preload: function(){
         this.game.load.image('mic', 'assets/images/mic_profile_icon.png');
+        this.game.load.image('mvd', 'assets/images/mvd_profile_icon.png');
     },
     create: function(){
         this.game.state.start("StartMenu");
