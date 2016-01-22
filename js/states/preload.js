82bf972b4c9dfdfba3569aed5abd927ab340e6aa
Mic <mikejcouture@gmail.com>
2016-01-22 13:03:49 -0500

    Added custom classes to use as templates

--- js/states/preload.js
+++ js/states/preload.js
@@ -6,6 +6,9 @@ Preload.prototype = {
 	init: function(params){
 	},
 	preload: function(){
+		// Use the script loader to avoid bloating the index with includes
+		//this.game.load.script('customGroup.js', 'js/customGroup.js');
+		//this.game.load.script('customSprite.js', 'js/customSprite.js');
 	},
 	create: function(){
 	},
