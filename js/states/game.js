2319268b99565dee97abe2e4c4a7e69388073f9b
Mic <mikejcouture@gmail.com>
2016-01-21 22:15:01 -0500

    Created an empty game structure. Just shows a black game screen

---
+++ js/states/game.js
@@ -0,0 +1,18 @@
+var GameState = function(game){
+};
+ 
+GameState.prototype = {
+	/* State methods */
+	init: function(params){
+	},
+	preload: function(){
+	},
+	create: function(){
+	},
+	update: function(){
+	},
+	render: function(){
+	},
+	shutdown: function(){
+	}
+}
---
+++ js/states/preload.js
@@ -0,0 +1,18 @@
+var Preload = function(game){
+};
+ 
+Preload.prototype = {
+	/* State methods */
+	init: function(params){
+	},
+	preload: function(){
+	},
+	create: function(){
+	},
+	update: function(){
+	},
+	render: function(){
+	},
+	shutdown: function(){
+	}
+}
