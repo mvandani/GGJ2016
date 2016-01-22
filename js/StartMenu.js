fac73749dfdea872fe025473e27cedd34fc3fa8c
Todd Lees <tlees@umd.edu>
2016-01-21 19:00:05 -0500

    todd's icon

--- js/StartMenu.js
+++ js/StartMenu.js
@@ -11,6 +11,7 @@ menu.prototype = {
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'mvd');
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'tnh');
         this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'kev');
+        this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.world.width), this.game.rnd.integerInRange(0, this.game.world.height), 'todd');
     },
     update: function(){
     },
