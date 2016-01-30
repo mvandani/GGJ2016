ada26827948413d1cd38989da3cfec45839df08d
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-30 00:19:35 -0500

    Add scrolling background

--- js/states/bgtest.js
+++ js/states/bgtest.js
@@ -1,15 +1,19 @@
 var BGTestState = function(game){
+	this.bgtile = null;
+	this.speed = 1;
 };
  
 BGTestState.prototype = {
 	init: function(params){
-		console.log("Hello world");
 	},
 	preload: function(){
 	},
 	create: function(){
+		var game = this.game;
+		this.bgtile = game.add.tileSprite(0, 0, this.scale.width, this.scale.height, "bgtile");
 	},
 	update: function(){
+		this.bgtile.tilePosition.x -= this.speed;
 	},
 	render: function(){
 	},
