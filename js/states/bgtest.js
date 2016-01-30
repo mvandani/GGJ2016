c769eb2bb1f22fba1323a9505c4d6c07ee77531a
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-29 23:34:06 -0500

    A separate state

---
+++ js/states/bgtest.js
@@ -0,0 +1,18 @@
+var BGTestState = function(game){
+};
+ 
+BGTestState.prototype = {
+	init: function(params){
+		console.log("Hello world");
+	},
+	preload: function(){
+	},
+	create: function(){
+	},
+	update: function(){
+	},
+	render: function(){
+	},
+	shutdown: function(){
+	}
+}
