1fe15092d12491de7da786ee29fa57aed2992a6b
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-30 00:58:50 -0500

    Move ground tile from world to mountain group

--- js/states/bgtest.js
+++ js/states/bgtest.js
@@ -1,17 +1,22 @@
 var BGTestState = function(game){
-	this.bgtile = null;
 	this.speed = 20;
+
+	this.mountain = null; // group of everything moving...
+	this.bgtile = null;
 };
  
 BGTestState.prototype = {
 	init: function(params){
+		this.stage.disableVisibilityChange = false; // no outside pause
 	},
 	preload: function(){
 	},
 	create: function(){
-		var game = this.game;
-		var tileHeight = game.cache.getImage('bgtile').height; // this.scale.height
-		this.bgtile = game.add.tileSprite(0, 0, this.scale.width, tileHeight, "bgtile");
+		this.mountain = this.add.group(this.world, "mountain");
+
+		var tileWidth = this.cache.getImage('bgtile').width; // this.scale.width
+		var tileHeight = this.cache.getImage('bgtile').height; // this.scale.height
+		this.bgtile = this.add.tileSprite(0, 0, tileWidth, tileHeight, "bgtile", null, this.mountain);
 	},
 	update: function(){
 		this.bgtile.tilePosition.x -= this.speed;
