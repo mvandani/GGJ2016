b19b6227b4c60634b17237423bd8e1c370a87af8
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-30 03:48:00 -0500

    Add slow mountains and static sky

--- js/states/bgtest.js
+++ js/states/bgtest.js
@@ -3,6 +3,7 @@ var BGTestState = function(game){
 
 	this.dispRoot = null; // group of everything moving...
 	this.ground = null;
+	this.mountains = null;
 };
  
 BGTestState.prototype = {
@@ -12,24 +13,30 @@ BGTestState.prototype = {
 	preload: function(){
 		this.load.image("ground", "assets/ground.png");
 		this.load.image("root", "assets/root-x.png");
+		this.load.image("mountains", "assets/mountains.png");
+		this.load.image("sky", "assets/sky.png");
 	},
 	create: function(){
 		var w = this.scale.width;
 		var h = this.scale.height;
-		var m = this.dispRoot = this.add.sprite(w / 2, h / 2, "root");
 
+		this.add.sprite(0, 0, "sky");
+		this.mountains = this.add.tileSprite(0, h - this.cache.getImage("mountains").height, w, h, "mountains");
+
+		var root = this.dispRoot = this.add.sprite(w / 2, h / 2, "root");
 		this.ground = this.make.tileSprite(-w * 2, 0, w * 4, h, "ground");
-		m.addChild(this.ground);
+		root.addChild(this.ground);
 
 		this.input.onDown.add(function(){
-			m.rotation -= 0.05;
+			root.rotation -= 0.05;
 		}, this);
 	},
 	update: function(){
 		this.ground.tilePosition.x += this.speed;
+		this.mountains.tilePosition.x += this.speed / 200;
 	},
 	render: function(){
-		this.game.debug.text(this.dispRoot.rotation, 10, 20);
+		this.game.debug.text(this.dispRoot.rotation, 10, 20, "#000");
 	},
 	shutdown: function(){
 	}
