530051ed1dad5d5aed419e2f46e90c1c8e97cf19
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-31 09:33:19 -0500

    Add replay button; just remove and re-add game state
    That instead of properly destroying/reseting the state.

--- js/states/ending.js
+++ js/states/ending.js
@@ -7,9 +7,19 @@ Ending.prototype = {
 	preload: function(){
 	},
 	create: function(){
+		var game = this.game;
+		game.state.remove("GameState");
+		game.state.add("GameState", GameState);
+
 		var w = this.scale.width;
 		var h = this.scale.height;
 		this.add.tileSprite(0, 0, w, h, "ending-bg");
+
+		// Play!
+		game.add.button(game.world.centerX - 95, game.world.centerY * 1.5, 'button', this.actionOnClick, this, 2, 1, 0);
+	},
+	actionOnClick: function(){
+		this.game.state.start("GameState");
 	},
 	update: function(){
 	},
