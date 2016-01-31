2948b0a2bc9ad74f03273d1fd05b567b3cf4a69b
Thach Hoang <thachhoang@users.noreply.github.com>
2016-01-31 01:49:50 -0500

    Add dead player, empty ending; state doesn't shutdown correctly
    Just reload the game if you want to play again.

---
+++ js/states/ending.js
@@ -0,0 +1,17 @@
+var Ending = function(game){
+};
+
+Ending.prototype = {
+	init: function(params){
+	},
+	preload: function(){
+	},
+	create: function(){
+	},
+	update: function(){
+	},
+	render: function(){
+	},
+	shutdown: function(){
+	}
+}
