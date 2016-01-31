c027165d8b91f8dde78c6437581c8ef60f0a7a5d
ltodd <mail@toddlees.com>
2016-01-31 14:06:13 -0500

    fixed last songs

--- js/states/ending.js
+++ js/states/ending.js
@@ -1,8 +1,7 @@
 var Ending = function(game){
 	this.params = null;
 	this.winScore = 10000;
-	this.goodSong;
-	this.badSong;
+	this.song;
 };
 
 Ending.prototype = {
@@ -15,8 +14,8 @@ Ending.prototype = {
 	preload: function(){
 	},
 	create: function(){
-		this.goodSong = this.game.add.audio('goodEnd');
-		this.badSong = this.game.add.audio('badEnd');
+		this.song = this.game.add.audio(this.params.score >= this.winScore ? 'goodEnd' : 'badEnd');
+
 		var game = this.game;
 		game.state.remove("GameState");
 		game.state.add("GameState", GameState);
@@ -26,7 +25,7 @@ Ending.prototype = {
 		this.add.tileSprite(0, 0, w, h, this.params.score >= this.winScore ? "win" : "loss");
 
 		// if(this.params.score >= this.winScore)
-			this.goodSong.play(0.5);
+			this.song.play();
 		// else
 		// 	this.badSong.play(0.5)
 
@@ -34,8 +33,7 @@ Ending.prototype = {
 		game.add.button(w - 250, h - 170, "retry_button", this.actionOnClick, this, 1, 0, 1);
 	},
 	actionOnClick: function(){
-		this.goodSong.stop();
-		this.badSong.stop();
+		this.song.stop();
 		this.game.state.start("GameState");
 	},
 	update: function(){
